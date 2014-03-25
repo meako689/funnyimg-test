9d7f8f0cca08262ee5f7f3ff0ac946cf30ffe6b1
Andrew Meakovski <meako689@gmail.com>
2014-03-25 10:42:35 +0200

    adapted for heroku

--- fimg/config/default.py
+++ fimg/config/default.py
@@ -1,8 +1,9 @@
+import os
 from os.path import dirname, normpath, join
 CONFIG_DIR = normpath(dirname(__file__))
 PROJ_ROOT = dirname(dirname(CONFIG_DIR))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_ECHO = False
 DEBUG = True
 
@@ -10,18 +11,18 @@ DATE_FORMAT = "%Y-%m-%d"
 TIME_FORMAT = "%H:%M:%S"
 DATETIME_FORMAT = DATE_FORMAT+" "+TIME_FORMAT
 
-#set up proper values in local.py
+#set up proper values in local.py or in env
 TUMBLR = {
-    'key':'',
-    'secret':'',
-    'token':'',
-    'token_secret':''
+    'key':os.environ.get('TUMBLR_KEY','' ),
+    'secret':os.environ.get('TUMBLR_SECRET','' ),
+    'token':os.environ.get('TUMBLR_TOKEN','' ),
+    'token_secret':os.environ.get('TUMBLR_TOKEN_SECRET','' )
 }
 
 INSTAGRAM = {
-    'token':'',
-    'id':'',
-    'secret':''
+    'token':os.environ.get('INSTAGRAM_TOKEN',''),
+    'id':os.environ.get('INSTAGRAM_ID',''),
+    'secret':os.environ.get('INSTAGRAM_SECRET','')
 }
 try:
     from .local import *
